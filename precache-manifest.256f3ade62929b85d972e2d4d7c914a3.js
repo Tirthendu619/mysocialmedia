50599c7a621559e7a4305df96c22c2c28ebf430a
Tirthendu Sahu- <tirthendu619@gmail.com>
2020-03-08 16:26:15 +0530

    Updates

---
+++ precache-manifest.256f3ade62929b85d972e2d4d7c914a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1481efde64e97195aa86bb4188df27d3",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "8df2e11977c64e73049a",
+    "url": "/social-network/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "b2d15d65ac4c268f76ce",
+    "url": "/social-network/static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "8df2e11977c64e73049a",
+    "url": "/social-network/static/js/2.eb2ddaf2.chunk.js"
+  },
+  {
+    "revision": "bd48e7aad8ffbde0152bdbc9a20111c5",
+    "url": "/social-network/static/js/2.eb2ddaf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d15d65ac4c268f76ce",
+    "url": "/social-network/static/js/main.51182f8d.chunk.js"
+  },
+  {
+    "revision": "16c0fda154a0eeaca1ef",
+    "url": "/social-network/static/js/runtime-main.16207f7b.js"
+  }
+]);
